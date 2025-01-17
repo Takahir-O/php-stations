3b5a573d49bbf13f03840dc9738bd4e2d2d4c85b
Takahir-O <orihack3553@gmail.com>
2025-01-18 01:25:38 +0900

    feat: Add sample code and tests for Station01 and Station02
    This commit adds sample code and tests for Station01 and Station02. In Station01/Practice.php, the code demonstrates the usage of variables and their types. In Station01/Question.php, the code initializes variables with specific values. In Station02/Practice.php, the code includes an if-else statement to check the value of a variable. Additionally, a new test output file, station01.xml, is added.
    
    Refs: #1

--- src/Station01/Practice.php
+++ src/Station01/Practice.php
@@ -7,6 +7,15 @@ class Practice
     public function main(): void
     {
         // ここにサンプルコードを記述
+        // $a = 'test';
+        // echo gettype($a).PHP_EOL;
+
+        $a = 1;
+        echo gettype($a) . PHP_EOL;
+
+        // 変数$bに対してboolになるような値を代入してください
+        $b = true;
+        echo gettype($b) . PHP_EOL;
     }
 }
 
--- src/Station02/Practice.php
+++ src/Station02/Practice.php
@@ -7,6 +7,14 @@ class Practice
     public function main(): void
     {
         // ここにサンプルコードを記述
+        $a =1;
+        $b = 2;
+
+        if($a >1){
+            echo '$aは1より大きい';
+        }else{
+            echo '$aは1以下';
+        }
     }
 }
 
