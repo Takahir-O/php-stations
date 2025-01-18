b843b5e34491eeeab4bbaf30c3a065a12620a571
Takahir-O <orihack3553@gmail.com>
2025-01-18 13:23:31 +0900

    Q2終了

--- src/Station02/Practice.php
+++ src/Station02/Practice.php
@@ -7,14 +7,31 @@ class Practice
     public function main(): void
     {
         // ここにサンプルコードを記述
-        $a =1;
-        $b = 2;
+        $a = 1;
+        $b = '1';
 
         if($a >1){
             echo '$aは1より大きい';
         }else{
             echo '$aは1以下';
         }
+
+        echo PHP_EOL;
+
+        if($a >=$b){
+            echo '$aは$b以上';
+        } else {
+            echo '$aは$b未満';
+        }
+
+        echo PHP_EOL;
+
+        if($a == $b){
+            echo '$aと$bは等しい';
+        } else {
+            echo '$aと$bは等しくない';
+        }
+        echo PHP_EOL;
     }
 }
 
--- src/Station02/Question.php
+++ src/Station02/Question.php
@@ -6,5 +6,22 @@ class Question
 {
     public function main(mixed $arg): string
     {
+        if (gettype($arg) === 'integer' && $arg === 0) {
+            return 'zero';
+        }
+
+        if (gettype($arg) === 'string' && $arg === '1') {
+            return 'foo';
+        }
+
+        if (gettype($arg) === 'integer' && $arg === 1) {
+            return 'bar';
+        }
+
+        if ((gettype($arg) === 'integer' || gettype($arg) === 'string') && $arg >= 2) {
+            return 'baz';
+        }
+
+        return 'others';
     }
 }
