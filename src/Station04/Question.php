59604fc3224d925579b64cf7c2506923716d0fe6
Takahir-O <orihack3553@gmail.com>
2025-01-21 00:06:28 +0900

    feat: Add sample code for Station04
    This commit adds sample code for Station04. The code includes a foreach loop that iterates over an array and breaks the loop when the value is equal to 5. It also includes a foreach loop in the Question class that calculates the sum of numbers in an array, skipping numbers that are multiples of 4 except for 4 itself.
    
    New test output file "station04.xml" is also added.

--- src/Station04/Question.php
+++ src/Station04/Question.php
@@ -6,5 +6,15 @@ class Question
 {
     public function main(): int
     {
+        $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $b = 0;
+        foreach ($array as $value) {
+            // 4以外の4の倍数をチェック
+            if ($value % 4 === 0 && $value !== 4) {
+                break;
+            }
+            $b = $b + $value;
+        }
+        return $b;
     }
 }
