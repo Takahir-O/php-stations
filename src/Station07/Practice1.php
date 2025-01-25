c10c00d8dc769b91818676d60361f66739833bfa
Takahir-O <orihack3553@gmail.com>
2025-01-25 21:48:07 +0900

    Station07のサンプルコードを追加

--- src/Station07/Practice1.php
+++ src/Station07/Practice1.php
@@ -7,6 +7,16 @@ class Practice1
     public function main(): void
     {
         // ここにサンプルコードを記述
+        $array2=[1,2,3];
+        $array1=[4,5];
+
+        $mergedArray = array_merge($array1, $array2);
+        print_r($mergedArray);
+
+        $array3 = ['apple' => 'red', 'banana' => 'yellow', 'rime' => 'green'];
+        $array4 = ['strawberry' => 'pink', 'banana' => 'yellow'];
+        $mergedArray2 = array_merge($array3, $array4);
+print_r($mergedArray2);
     }
 }
 
--- src/Station07/Question.php
+++ src/Station07/Question.php
@@ -6,15 +6,24 @@ class Question
 {
     public function one(): array
     {
-        $array1 = ['北海道' => 1, '東京都' => 13, '大阪府' => 'XX'];
+        $array1 = ['北海道' => 1, '東京都' => 13, '大阪府' => 27];
         $array2 = ['広島県' => 34, '京都府' => 26];
-        $array3 = ['京都府' => 'XX', '大阪府' => 27];
+        $array3 = ['京都府' => 26, '大阪府' => 27];
+        $array4 = array_merge($array1,$array2,$array3);
+        return $array4;
+
+
     }
 
     public function two(): array
     {
         $firstNames = ['太郎', '次郎', '花子'];
         $lastNames = ['山田', '鈴木', '佐藤'];
+        // lastNamesとfirstNamesを結合して、['山田太郎', '鈴木次郎', '佐藤花子']という配列を作成する
+        $array = array_map(function($lastNames,$firstNames){
+            return $lastNames.$firstNames;
+        },$lastNames,$firstNames);
+        return $array;
     }
 
     public function three(): array
@@ -24,5 +33,8 @@ class Question
             ['name' => 'banana', 'price' => 200],
             ['name' => 'orange', 'price' => 120],
         ];
+
+        $array2 = array_column($array,'price','name');
+        return $array2;
     }
 }
